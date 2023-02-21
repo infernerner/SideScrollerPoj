e971519e0cd617ea2e877e640d91111cec6dffa2
infernerner <101186811+infernerner@users.noreply.github.com>
2023-02-21 10:58:29 +0100

    fish fish

--- SideScroller/Assets/scripts/fish/predatorFish.cs
+++ SideScroller/Assets/scripts/fish/predatorFish.cs
@@ -10,28 +10,33 @@ public class predatorFish : MonoBehaviour
     public int FishTier = 1;
     public float swimSpeed = 200;
     public float turnSpeed = 100;
+    public Transform turnTransform;
+    public GameObject target;
     private void Start()
     {
         myRB = GetComponent<Rigidbody>();
     }
     void FixedUpdate()
     {
-        if (Physics.OverlapSphere(transform.position,10) != null)
-        {
-            Debug.Log("a");
-            if (myEyes.collider.GetComponent<predatorFish>().FishTier <= FishTier)
-            {
-                myRB.AddForce(transform.up * swimSpeed);
-                myRB.AddTorque(transform.forward * turnSpeed * Vector3.Angle(transform.position, myEyes.collider.transform.position) * Mathf.Sin(Time.fixedTime));
-            }
-
-        }
-        else
-        {
-            myRB.AddForce(transform.up * swimSpeed);
-            myRB.AddTorque(transform.forward * turnSpeed * Random.Range(-1f, 1f) * Mathf.Sin(Time.fixedTime));
-        }
-
-
+        Debug.Log(Quaternion.RotateTowards(transform.rotation,turnTransform.rotation,180));
+        turnTransform.LookAt(target.transform, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, turnTransform.rotation, 180);
+        //if (Physics.OverlapSphere(transform.position,10) != null)
+        //{
+        //
+        //    if (myEyes.collider.GetComponent<predatorFish>().FishTier <= FishTier)
+        //    {
+        //        myRB.AddForce(transform.up * swimSpeed);
+        //        
+        //    }
+        //
+        //}
+        //else
+        //{
+        //    myRB.AddForce(transform.up * swimSpeed);
+        //    myRB.AddTorque(transform.forward * turnSpeed * Random.Range(-1f, 1f) * Mathf.Sin(Time.fixedTime));
+        //}
+        //
+        //
     }
 }
