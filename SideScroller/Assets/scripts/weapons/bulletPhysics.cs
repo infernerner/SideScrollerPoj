23ff5b1dfed59be708a5c199ab15573f64fb42a7
infernerner <101186811+infernerner@users.noreply.github.com>
2023-03-08 09:56:40 +0100

    fixed bullet physics to activate in water

--- SideScroller/Assets/scripts/weapons/bulletPhysics.cs
+++ SideScroller/Assets/scripts/weapons/bulletPhysics.cs
@@ -5,36 +5,35 @@ using UnityEngine;
 public class bulletPhysics : MonoBehaviour
 {
     private Rigidbody myRB;
+    private RaycastHit water;
 
     private void Start()
     {
         myRB = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         if (myRB.velocity.magnitude > 2f)
         transform.LookAt(myRB.velocity + transform.position, Vector3.up);
-
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.transform.tag == "Water")
+        Physics.Raycast(transform.position, Vector3.forward, out water);
+        if (water.collider == null)
+        {
+            myRB.drag = 1f;
+            myRB.angularDrag = 1f;
+        }
+        else if (water.collider.tag != "Water")
+        {
+            myRB.drag = 1f;
+            myRB.angularDrag = 1f;
+        }
+        else if (water.collider.tag == "Water")
         {
             myRB.drag = 2f;
             myRB.angularDrag = 2f;
         }
     }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.transform.tag == "Water")
-        {
-            myRB.drag = 1f;
-            myRB.angularDrag = 1f;
-        }
-    }
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.GetComponent<predatorFish>())
